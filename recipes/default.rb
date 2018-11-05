cacd92687ac33167aec34f5a0f079b1f4d7c0f39
Amrit Dhungana <amrit.dhungana@ibm.com@amrits-mbp.ustx.ibm.com>
2018-11-05 09:55:53 -0600

    include apt update, audit package to install and change template

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'auditd' do
+  action :install
+end
+
+template '/etc/audit/auditd.conf' do
+  source 'auditd.conf.erb'
+  notifies :restart, 'service[auditd]'
+end
+
+service 'auditd' do
+  action :nothing
+end
